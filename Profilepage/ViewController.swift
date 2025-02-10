e077620c1d122212ac631d2c79b14afe353e55a0
Purv-sinojiya-inventyv <purv.sinojiya.inventyv@gmail.com>
2025-02-10 15:03:24 +0530

    Initial Commit

---
+++ Profilepage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profilepage
+//
+//  Created by Purv Sinojiya on 10/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
